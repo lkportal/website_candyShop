8c5028bc6b8fccdb79cff893e85636e7e7d45633
lkportal <lksxim@gmail.com>
2023-10-05 17:14:47 -0300

    add new project 001

---
+++ pages/compra.tsx
@@ -0,0 +1,7 @@
+export default function Compra(){
+    return(
+        <div>
+            <h1>Compra</h1>
+        </div>
+    )
+}
---
+++ pages/receitas.tsx
@@ -0,0 +1,7 @@
+export default function Receitas(){
+    return(
+        <div>
+            <h1>receitas</h1>
+        </div>
+    )
+}
